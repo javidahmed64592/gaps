eb8c18e877e25a03038547d9006457d4f4c8b796
Javid Ahmed <javidahmed@icloud.com>
2023-04-14 23:47:35 +0100

    Add genetic algorithm

--- gaps_api/src/routers/ga.py
+++ gaps_api/src/routers/ga.py
@@ -2,8 +2,16 @@ from flask import request
 from __main__ import app
 from utils.router_utils import make_request
 from utils.ga_utils import parse_gene_pools
+from models.population import population
+
 
 @app.route("/start_algorithm", methods=["POST"])
-def press():
-    print(request.json["phrase"], parse_gene_pools(request.json["genes"]), request.json["populationSize"], request.json["mutationRate"])
+def start_algorithm():
+    phrase = request.json["phrase"]
+    genes = parse_gene_pools(request.json["genes"])
+    population_size = request.json["populationSize"]
+    mutation_rate = request.json["mutationRate"]
+    mypop = population(population_size, phrase, genes, mutation_rate)
+    mypop.run()
+
     return make_request("Success", 200)
